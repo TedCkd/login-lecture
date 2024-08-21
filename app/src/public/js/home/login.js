99097cfd0e799308daf384e7bf5e62b3d897aacf
Ted Chang <ckdtlrdl78@naver.com>
2024-08-21 20:50:17 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,6 +19,8 @@ function login() {
         headers : {
             "Content-Type": "application/json",
         },
-        body : JSON.stringify(req)
+        body : JSON.stringify(req),
     })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 }
