f6c34d33e923cbac3b3dbd783aa2d11006554a17
Ted Chang <ckdtlrdl78@naver.com>
2024-08-22 14:25:27 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -12,9 +12,22 @@ class UserStorage {
         const newUsers = fields.reduce((newUsers, field)=>{
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
-            }return newUsers;
+            }
+            return newUsers;
         }, {});
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKey = Object.keys(users);
+        const userInfo = usersKey.reduce((newUsers, info) => {
+            newUsers[info] = users[info][idx];
+            return newUsers;
+        }, {});
+
+        return userInfo;
     }
 }
 
